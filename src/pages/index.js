c23efd7a64ed3e907d260b61c0dc33250bcabd34
Katherine Huang <hello@katmh.com>
2019-09-27 00:04:00 -0400

    Add Theme UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,26 @@
-import React from "react"
+/** @jsx jsx */
 import { graphql } from "gatsby"
 import Header from "../components/Header"
+import { jsx, ThemeProvider } from 'theme-ui'
+import theme from "../components/theme"
 
 const IndexPage = ({ data }) => (
-    <>
+    <ThemeProvider theme={theme}>
         <Header />
         {data.allAirtable.edges.map((edge, i) => (
             <div>
-                <h2>
-                  {edge.node.data.Story_Name}
+                <h2
+                    sx={{
+                        color: 'text',
+                        fontFamily: 'body',
+                    }}>
+                    {edge.node.data.Story_Name}
                 </h2>
                 <p>By {edge.node.data.Author}</p>
                 <img alt={edge.node.data.Author} src={edge.node.data.Photo[0].url} />
             </div>
         ))}
-    </>
+    </ThemeProvider>
 )
 
 export default IndexPage
