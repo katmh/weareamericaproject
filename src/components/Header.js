d60de3731904268011058eaed000e30b52fef6f2
Katherine Huang <hello@katmh.com>
2019-09-26 23:50:26 -0400

    Add some content to index.js

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => (
+    <header>
+        <a href="https://lhsweareamerica.com">
+            <h1>We Are America</h1>
+        </a>
+
+        <h2>Voices of the Nation's Future</h2>
+
+        <nav>
+            <li><a href="about">About</a></li>
+            <li><a href="contact">Contact</a></li>
+            <li><a href="https://instagram.com/lhsweareamerica">Instagram</a></li>
+        </nav>
+    </header>
+)
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,42 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Header from "../components/Header"
 
-export default () => <div>Hello world!</div>
+const IndexPage = ({ data }) => (
+    <>
+        <Header />
+        {data.allAirtable.edges.map((edge, i) => (
+            <div>
+                <h2>
+                  {edge.node.data.Story_Name}
+                </h2>
+                <p>By {edge.node.data.Author}</p>
+                <img alt={edge.node.data.Author} src={edge.node.data.Photo[0].url} />
+            </div>
+        ))}
+    </>
+)
+
+export default IndexPage
+
+export const query = graphql`
+{
+    allAirtable {
+      edges {
+        node {
+          data {
+            Audio {
+              url
+            }
+            Author
+            Photo {
+              url
+            }
+            School_Class
+            Story_Name
+          }
+        }
+      }
+    }
+  }
+`
