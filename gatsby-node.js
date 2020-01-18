e69ede1c20e64c2243d654e569fc422915f2cb53
Katherine Huang <hello@katmh.com>
2020-01-17 21:36:31 -0500

    Add support for second language

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,203 +1,213 @@
 function slugify(string) {
-    const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
-    const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------'
-    const p = new RegExp(a.split('').join('|'), 'g')
-  
-    return string.toString().toLowerCase()
-      .replace(/\s+/g, '-') // Replace spaces with -
-      .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
-      .replace(/&/g, '-and-') // Replace & with 'and'
-      .replace(/[^\w\-]+/g, '') // Remove all non-word characters
-      .replace(/\-\-+/g, '-') // Replace multiple - with single -
-      .replace(/^-+/, '') // Trim - from start of text
-      .replace(/-+$/, '') // Trim - from end of text
-  }
+  const a =
+    "àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;"
+  const b =
+    "aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------"
+  const p = new RegExp(a.split("").join("|"), "g")
+
+  return string
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, "-and-") // Replace & with 'and'
+    .replace(/[^\w\-]+/g, "") // Remove all non-word characters
+    .replace(/\-\-+/g, "-") // Replace multiple - with single -
+    .replace(/^-+/, "") // Trim - from start of text
+    .replace(/-+$/, "") // Trim - from end of text
+}
 
 exports.createPages = async ({ graphql, actions }) => {
-    const { createPage } = actions
-    const stories = await graphql(`
-      query allStoriesQuery {
-          allAirtable(filter: {table: {eq: "Stories"}}) {
-              edges {
-              node {
-                  id
-                  data {
-                      Author
-                      Status
-                      Story_Name
-                      Photo {
-                          thumbnails {
-                            large {
-                                url
-                            }
-                          }
-                      }
-                      Audio {
-                          url
-                      }
-                      School
-                      Tags
-                      Grade
-                      Transcript
-                    }
-                  }
-              }
-          }
-      }
-    `)
-    const tags = await graphql(`
-      query storiesByTagsQuery {
-        allAirtable(filter: {table: {eq: "Stories"}}) {
-          group(field: data___Tags) {
-            edges {
-              node {
-                id
-                data {
-                  Author
-                  Status
-                  Story_Name
-                  Photo {
-                    thumbnails {
-                      large {
-                        url
-                      }
-                    }
-                  }
-                  Audio {
+  const { createPage } = actions
+  const stories = await graphql(`
+    query allStoriesQuery {
+      allAirtable(filter: { table: { eq: "Stories" } }) {
+        edges {
+          node {
+            id
+            data {
+              Author
+              Status
+              Story_Name
+              Photo {
+                thumbnails {
+                  large {
                     url
                   }
-                  School
-                  Tags
-                  Grade
-                  Transcript
                 }
               }
+              Audio {
+                url
+              }
+              Second_Language
+              Second_Language_Audio {
+                url
+              }
+              School
+              Tags
+              Grade
+              Transcript
             }
-            fieldValue
           }
         }
       }
-    `)
-    const schools = await graphql(`
-      query storiesByTagsQuery {
-        allAirtable(filter: {table: {eq: "Stories"}}) {
-          group(field: data___School) {
-            edges {
-              node {
-                id
-                data {
-                  Author
-                  Status
-                  Story_Name
-                  Photo {
-                    thumbnails {
-                      large {
-                        url
-                      }
-                    }
-                  }
-                  Audio {
-                    url
-                  }
-                  School
-                  Tags
-                  Grade
-                  Transcript
-                }
-              }
-            }
-            fieldValue
-          }
-        }
-      }
-    `)
-    const states = await graphql(`
-      query storiesByTagsQuery {
-        allAirtable(filter: {table: {eq: "Stories"}}) {
-          group(field: data___State) {
-            edges {
-              node {
-                id
-                data {
-                  Author
-                  Status
-                  Story_Name
-                  Photo {
-                    thumbnails {
-                      large {
-                        url
-                      }
-                    }
-                  }
-                  Audio {
-                    url
-                  }
-                  School
-                  Tags
-                  Grade
-                  Transcript
-                }
-              }
-            }
-            fieldValue
-          }
-        }
-      }
-    `)
-    const blogPosts = await graphql(`
-      query allBlogPosts {
-        allMarkdownRemark {
+    }
+  `)
+  const tags = await graphql(`
+    query storiesByTagsQuery {
+      allAirtable(filter: { table: { eq: "Stories" } }) {
+        group(field: data___Tags) {
           edges {
             node {
-              frontmatter {
-                path
-                title
-                date(formatString: "MMMM DD, YYYY")
+              id
+              data {
+                Author
+                Status
+                Story_Name
+                Photo {
+                  thumbnails {
+                    large {
+                      url
+                    }
+                  }
+                }
+                Audio {
+                  url
+                }
+                School
+                Tags
+                Grade
+                Transcript
               }
-              html
             }
+          }
+          fieldValue
+        }
+      }
+    }
+  `)
+  const schools = await graphql(`
+    query storiesByTagsQuery {
+      allAirtable(filter: { table: { eq: "Stories" } }) {
+        group(field: data___School) {
+          edges {
+            node {
+              id
+              data {
+                Author
+                Status
+                Story_Name
+                Photo {
+                  thumbnails {
+                    large {
+                      url
+                    }
+                  }
+                }
+                Audio {
+                  url
+                }
+                School
+                Tags
+                Grade
+                Transcript
+              }
+            }
+          }
+          fieldValue
+        }
+      }
+    }
+  `)
+  const states = await graphql(`
+    query storiesByTagsQuery {
+      allAirtable(filter: { table: { eq: "Stories" } }) {
+        group(field: data___State) {
+          edges {
+            node {
+              id
+              data {
+                Author
+                Status
+                Story_Name
+                Photo {
+                  thumbnails {
+                    large {
+                      url
+                    }
+                  }
+                }
+                Audio {
+                  url
+                }
+                School
+                Tags
+                Grade
+                Transcript
+              }
+            }
+          }
+          fieldValue
+        }
+      }
+    }
+  `)
+  const blogPosts = await graphql(`
+    query allBlogPosts {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              path
+              title
+              date(formatString: "MMMM DD, YYYY")
+            }
+            html
           }
         }
       }
-    `)
+    }
+  `)
 
-    stories.data.allAirtable.edges.forEach(({node: {id, data}}) => {
-        createPage({
-            path: `/story/${slugify(data.Author ? data.Author : id)}/`,
-            component: require.resolve('./src/templates/story.js'),
-            context: {data},
-        })
+  stories.data.allAirtable.edges.forEach(({ node: { id, data } }) => {
+    createPage({
+      path: `/story/${slugify(data.Author ? data.Author : id)}/`,
+      component: require.resolve("./src/templates/story.js"),
+      context: { data },
     })
+  })
 
-    tags.data.allAirtable.group.forEach(({fieldValue, edges}) => {
-        createPage({
-            path: `/tag/${slugify(fieldValue ? fieldValue : '')}/`,
-            component: require.resolve('./src/templates/tag.js'),
-            context: {edges, fieldValue},
-        })
+  tags.data.allAirtable.group.forEach(({ fieldValue, edges }) => {
+    createPage({
+      path: `/tag/${slugify(fieldValue ? fieldValue : "")}/`,
+      component: require.resolve("./src/templates/tag.js"),
+      context: { edges, fieldValue },
     })
+  })
 
-    schools.data.allAirtable.group.forEach(({fieldValue, edges}) => {
-      createPage({
-          path: `/school/${slugify(fieldValue ? fieldValue : '')}/`,
-          component: require.resolve('./src/templates/tag.js'),
-          context: {edges, fieldValue},
-      })
+  schools.data.allAirtable.group.forEach(({ fieldValue, edges }) => {
+    createPage({
+      path: `/school/${slugify(fieldValue ? fieldValue : "")}/`,
+      component: require.resolve("./src/templates/tag.js"),
+      context: { edges, fieldValue },
     })
+  })
 
-    states.data.allAirtable.group.forEach(({fieldValue, edges}) => {
-      createPage({
-          path: `/state/${slugify(fieldValue ? fieldValue : '')}/`,
-          component: require.resolve('./src/templates/tag.js'),
-          context: {edges, fieldValue},
-      })
+  states.data.allAirtable.group.forEach(({ fieldValue, edges }) => {
+    createPage({
+      path: `/state/${slugify(fieldValue ? fieldValue : "")}/`,
+      component: require.resolve("./src/templates/tag.js"),
+      context: { edges, fieldValue },
     })
-    
-    blogPosts.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: `/news/${slugify(node.frontmatter.path ? node.frontmatter.path : '')}/`,
-        component: require.resolve('./src/templates/blog-post.js'),
-        context: {node},
-      })
+  })
+
+  blogPosts.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: `/news/${slugify(
+        node.frontmatter.path ? node.frontmatter.path : ""
+      )}/`,
+      component: require.resolve("./src/templates/blog-post.js"),
+      context: { node },
     })
+  })
 }
--- src/templates/story.js
+++ src/templates/story.js
@@ -1,122 +1,156 @@
 /** @jsx jsx */
-import Layout from '../components/Layout'
-import { jsx } from 'theme-ui'
-import BackToAll from '../components/BackToAll'
-import { Link } from 'gatsby'
+import Layout from "../components/Layout"
+import { jsx } from "theme-ui"
+import BackToAll from "../components/BackToAll"
+import { Link } from "gatsby"
 
 function slugify(string) {
-    const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
-    const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------'
-    const p = new RegExp(a.split('').join('|'), 'g')
-  
-    return string.toString().toLowerCase()
-      .replace(/\s+/g, '-') // Replace spaces with -
-      .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
-      .replace(/&/g, '-and-') // Replace & with 'and'
-      .replace(/[^\w\-]+/g, '') // Remove all non-word characters
-      .replace(/\-\-+/g, '-') // Replace multiple - with single -
-      .replace(/^-+/, '') // Trim - from start of text
-      .replace(/-+$/, '') // Trim - from end of text
-  }
+  const a =
+    "àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;"
+  const b =
+    "aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------"
+  const p = new RegExp(a.split("").join("|"), "g")
 
-export default ({pageContext: {data}}) => (
-    <Layout>
-        <BackToAll name="stories" path="/stories" />
-        <h1
-            sx={{
-                fontFamily: 'heading',
-                fontSize: 5,
-                color: 'accent'
-            }}
-        >
-            {data.Story_Name}
-        </h1>
-        <h2
-            sx={{
-                fontFamily: 'heading',
-                fontSize: 3,
-                textTransform: 'uppercase',
-                color: 'text'
-            }}
-        >
-            By {data.Author}
-        </h2>
-        <h3
-            sx={{
-                fontFamily: 'heading',
-                my: 3,
-                fontSize: 2,
-                color: 'muted'
-            }}
-        >
-            {data.School}
-        </h3>
-        <p>
-            {data.Tags ?
-                <ul>
-                    <span
-                        sx={{
-                            fontFamily: 'body',
-                            fontWeight: '700',
-                            mr: 1
-                        }}
-                    >
-                        Tags:
-                    </span>
-                    {data.Tags.map((tag) => (
-                        <li
-                            sx={{
-                                listStyle: 'none',
-                                display: 'inline-block',
-                                mr: 1,
-                            }}
-                        >
-                            <Link
-                                to={'/tag/' + slugify(tag)}
-                                sx={{
-                                    fontFamily: 'body',
-                                    lineHeight: '100%',
-                                    textDecoration: 'none',
-                                    bg: 'accent',
-                                    color: 'background',
-                                    p: '.4rem .6rem',
-                                    borderRadius: '.3rem',
-                                    fontSize: 0,
-                                    fontWeight: '700',
-                                    transition: '.1s',
-                                    display: 'inline-block',
-                                    my: '.15rem',
-                                    ':hover': {
-                                        color: '#fff',
-                                        background: '#844'
-                                    }
-                                }}
-                            >
-                                {tag}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            : ''
-            }
-        </p>
+  return string
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, "-and-") // Replace & with 'and'
+    .replace(/[^\w\-]+/g, "") // Remove all non-word characters
+    .replace(/\-\-+/g, "-") // Replace multiple - with single -
+    .replace(/^-+/, "") // Trim - from start of text
+    .replace(/-+$/, "") // Trim - from end of text
+}
 
-        <audio
-            controls
-            src={data.Audio ? data.Audio[0].url : ''}
+export default ({ pageContext: { data } }) => (
+  <Layout>
+    <BackToAll name="stories" path="/stories" />
+    <h1
+      sx={{
+        fontFamily: "heading",
+        fontSize: 5,
+        color: "accent",
+      }}
+    >
+      {data.Story_Name}
+    </h1>
+    <h2
+      sx={{
+        fontFamily: "heading",
+        fontSize: 3,
+        textTransform: "uppercase",
+        color: "text",
+      }}
+    >
+      By {data.Author}
+    </h2>
+    <h3
+      sx={{
+        fontFamily: "heading",
+        my: 3,
+        fontSize: 2,
+        color: "muted",
+      }}
+    >
+      {data.School}
+    </h3>
+    <p>
+      {data.Tags ? (
+        <ul>
+          <span
             sx={{
-                display: 'block',
-                my: 4
+              fontFamily: "body",
+              fontWeight: "700",
+              mr: 1,
             }}
-        >
-            Your browser does not support the <code>audio</code> element. :(
-        </audio>
-        <img
-            src={data.Photo ? data.Photo[0].thumbnails.large.url : ''}
-            alt={'Photo of' + data.Author}
-            sx={{
-                maxWidth: '100%'
-            }}
-        />
-    </Layout>
+          >
+            Tags:
+          </span>
+          {data.Tags.map(tag => (
+            <li
+              sx={{
+                listStyle: "none",
+                display: "inline-block",
+                mr: 1,
+              }}
+            >
+              <Link
+                to={"/tag/" + slugify(tag)}
+                sx={{
+                  fontFamily: "body",
+                  lineHeight: "100%",
+                  textDecoration: "none",
+                  bg: "accent",
+                  color: "background",
+                  p: ".4rem .6rem",
+                  borderRadius: ".3rem",
+                  fontSize: 0,
+                  fontWeight: "700",
+                  transition: ".1s",
+                  display: "inline-block",
+                  my: ".15rem",
+                  ":hover": {
+                    color: "#fff",
+                    background: "#844",
+                  },
+                }}
+              >
+                {tag}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        ""
+      )}
+    </p>
+
+    <audio
+      controls
+      src={data.Audio ? data.Audio[0].url : ""}
+      sx={{
+        display: "block",
+        my: 4,
+      }}
+    >
+      Your browser does not support the <code>audio</code> element. :(
+    </audio>
+
+    {data.Second_Language ? (
+      <span
+        sx={{
+          fontFamily: "body",
+          fontWeight: "700",
+          mr: 1,
+          verticalAlign: "top",
+        }}
+      >
+        Listen in {data.Second_Language}
+      </span>
+    ) : null}
+    {data.Second_Language ? (
+      <audio
+        controls
+        src={
+          data.Second_Language_Audio ? data.Second_Language_Audio[0].url : ""
+        }
+        sx={{
+          display: "block",
+          mt: 2,
+          mb: 4,
+        }}
+      >
+        Your browser does not support the <code>audio</code> element. :(
+      </audio>
+    ) : null}
+
+    <img
+      src={data.Photo ? data.Photo[0].thumbnails.large.url : ""}
+      alt={"Photo of" + data.Author}
+      sx={{
+        maxWidth: "100%",
+      }}
+    />
+  </Layout>
 )
