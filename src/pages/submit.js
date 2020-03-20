f9e70b14f69f86b7f89cbd09b0132de3514e915a
Katherine Huang <hello@katmh.com>
2020-03-20 13:43:54 -0400

    Add submit page that redirects to story submission form

---
+++ src/pages/submit.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+if (typeof window !== `undefined`) window.location.replace(`https://airtable.com/shraBPEf6TXdRZbUU`)
+
+const IndexPage = () => {
+    return <></>
+}
+
+export default IndexPage
