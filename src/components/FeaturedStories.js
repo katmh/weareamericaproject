521596eb20a815918d59118e3c28fd7f9fa916e9
Eric Zhang <ekzhang1@gmail.com>
2021-06-19 18:22:07 -0400

    Try to fix build, expand swap statement
    Netlify CI is showing me a build error, possibly since the webpack configuration doesn't support ES6 syntax?

--- src/components/FeaturedStories.js
+++ src/components/FeaturedStories.js
@@ -7,7 +7,9 @@ import React, { useMemo, useEffect } from "react"
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
-    [array[i], array[j]] = [array[j], array[i]]
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
   }
 }
 
