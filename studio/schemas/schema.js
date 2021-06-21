f7b0115e94748dd8311477d17443834dd441b853
katmh <hello@katmh.com>
2021-06-20 21:20:39 -0400

    About page schema

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -12,5 +12,45 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
+    {
+      title: 'About Page',
+      name: 'about',
+      type: 'document',
+      fields: [
+        {
+          title: 'Title',
+          name: 'title',
+          type: 'string',
+        },
+        {
+          title: 'Body Text',
+          name: 'body',
+          type: 'array',
+          of: [{type: 'block'}]
+        },
+        {
+          title: 'Partners',
+          name: 'partners',
+          type: 'array',
+          of: [{
+            title: 'Partner',
+            name: 'partner',
+            type: 'object',
+            fields: [
+              {
+                title: 'Partner Name',
+                name: 'name',
+                type: 'string',
+              },
+              {
+                title: 'Logo',
+                name: 'logo',
+                type: 'image',
+              },
+            ]
+          }]
+        }
+      ]
+    }
   ]),
 })
