1a882629c93f854724a0a054d5ab376955c5590a
katmh <hello@katmh.com>
2021-08-05 00:33:14 -0400

    Configure studio dashboard; add package names

--- studio/src/dashboardConfig.js
+++ studio/src/dashboardConfig.js
@@ -1,57 +1,34 @@
 export default {
   widgets: [
-    { name: "structure-menu" },
     {
-      name: "project-info",
+      name: "structure-menu",
+      layout: {
+        width: "full",
+      },
+    },
+    {
+      name: "netlify",
+      layout: {
+        width: "full",
+      },
       options: {
-        __experimental_before: [
+        sites: [
+          // TODO: use correct build hook IDs, Netlify site names, and API IDs
           {
-            name: "netlify",
-            options: {
-              description:
-                "NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.",
-              sites: [
-                {
-                  buildHookId:
-                    "6109a6fe7fe03d00b3ca57b9",
-                  title: "Sanity Studio",
-                  name: "sanity-gatsby-blog-studio-d48352sg",
-                  apiId: "6fdca64d-8908-44cd-96de-15527630fd62",
-                },
-                {
-                  buildHookId: "6109a6fe0b2c3700c247e0a0",
-                  title: "Blog Website",
-                  name: "sanity-gatsby-blog-web-qq95ktt1",
-                  apiId: "a035bc43-5b9f-4b1d-99b0-47a80007b84f",
-                },
-              ],
-            },
-          },
-        ],
-        data: [
-          {
-            title: "GitHub repo",
-            value:
-              "https://github.com/katmh/sanity-gatsby-blog",
-            category: "Code",
+            buildHookId: "6109a6fe7fe03d00b3ca57b9",
+            title: "Content Management System ",
+            name: "sanity-gatsby-blog-studio-d48352sg",
+            apiId: "6fdca64d-8908-44cd-96de-15527630fd62",
           },
           {
-            title: "Frontend",
-            value: "https://sanity-gatsby-blog-web-qq95ktt1.netlify.app",
-            category: "apps",
+            buildHookId: "6109a6fe0b2c3700c247e0a0",
+            title: "weareamericaproject.com ",
+            name: "sanity-gatsby-blog-web-qq95ktt1",
+            apiId: "a035bc43-5b9f-4b1d-99b0-47a80007b84f",
           },
         ],
       },
     },
-    { name: "project-users", layout: { height: "auto" } },
-    {
-      name: "document-list",
-      options: {
-        title: "Recent blog posts",
-        order: "_createdAt desc",
-        types: ["post"],
-      },
-      layout: { width: "medium" },
-    },
+    
   ],
 };
