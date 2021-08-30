056d36d4c698d6487e59b92e0b13cce4f3884c5a
katmh <hello@katmh.com>
2021-08-29 21:28:06 -0400

    Tooltips and mouse events

--- web/src/components/Map.js
+++ web/src/components/Map.js
@@ -1,4 +1,5 @@
 /** @jsx jsx */
+import { useState } from "react"
 import { jsx } from "theme-ui"
 import { graphql, useStaticQuery } from "gatsby"
 import { SVGMap } from "react-svg-map"
@@ -24,12 +25,52 @@ const Map = () => {
   const unique = cleaned.filter((state, i, arr) => arr.indexOf(state) === i)
   unique.sort()
 
+  const [tooltip, setTooltip] = useState({
+    pointedLocation: null,
+    style: {
+      display: "none",
+    },
+  })
+
   const getLocationClassName = (location) =>
-    `state ${unique.includes(location.name) && "active"}`
+    `state${unique.includes(location.name) ? " active" : ""}`
+
+  const handleLocationMouseOver = (e) =>
+    setTooltip({ ...tooltip, pointedLocation: e.target.attributes.name.value })
+
+  const handleLocationMouseOut = () => {
+    setTooltip({
+      pointedLocation: null,
+      style: {
+        display: "none",
+      },
+    })
+  }
+
+  const handleLocationMouseMove = (e) => {
+    console.log(e.clientY, e.clientX)
+    setTooltip({
+      ...tooltip,
+      style: {
+        display: "block",
+        top: e.clientY + 15,
+        left: e.clientX,
+      },
+    })
+  }
 
   return (
     <div>
-      <SVGMap map={USA} locationClassName={getLocationClassName} />
+      <SVGMap
+        map={USA}
+        locationClassName={getLocationClassName}
+        onLocationMouseOver={handleLocationMouseOver}
+        onLocationMouseMove={handleLocationMouseMove}
+        onLocationMouseOut={handleLocationMouseOut}
+      />
+      <div className="tooltip" style={tooltip.style}>
+        {tooltip.pointedLocation}
+      </div>
     </div>
   )
 }
