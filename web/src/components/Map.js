25dab836925bc278efedeece8ae72e5609125c2a
katmh <hello@katmh.com>
2021-08-29 20:39:21 -0400

    Color-coded USA map

--- web/src/components/Map.js
+++ web/src/components/Map.js
@@ -1,6 +1,9 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
 import { graphql, useStaticQuery } from "gatsby"
+import { SVGMap } from "react-svg-map"
+import USA from "@svg-maps/usa"
+import "./Map.css"
 
 const Map = () => {
   const data = useStaticQuery(graphql`
@@ -17,14 +20,16 @@ const Map = () => {
     }
   `)
   const states = data.allAirtable.edges.map((edge) => edge.node.data.State)
-  console.log(states)
   const cleaned = states.filter((state) => state !== null)
   const unique = cleaned.filter((state, i, arr) => arr.indexOf(state) === i)
   unique.sort()
 
+  const getLocationClassName = (location) =>
+    `state ${unique.includes(location.name) && "active"}`
+
   return (
     <div>
-      <p>hi</p>
+      <SVGMap map={USA} locationClassName={getLocationClassName} />
     </div>
   )
 }
