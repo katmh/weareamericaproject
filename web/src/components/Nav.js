b5c6b165007d671ee2582561eb02d36dce27d0a2
katmh <hello@katmh.com>
2022-07-18 22:32:26 -0700

    Nav submenus

--- web/src/components/Nav.js
+++ web/src/components/Nav.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { graphql, Link, useStaticQuery } from "gatsby";
+import ChevronDown from "../components/icons/ChevronDown";
 import "../styles/components/nav.scss";
 
 const query = graphql`
@@ -18,6 +19,16 @@ const query = graphql`
   }
 `;
 
+/*
+nav
+  div.top_level_item [hover to make submenu appear]
+    a (gatsby <Link />)
+      span
+      (chevron)
+    div.submenu [display: none]
+      a.submenu_item
+*/
+
 const Nav = () => {
   const data = useStaticQuery(query);
   const { navItems } = data.sanitySiteSettings;
@@ -25,14 +36,27 @@ const Nav = () => {
   return (
     <nav>
       {navItems.map(item => {
+        const hasSubmenu = item.subItems.length > 0;
         return (
-          <Link
-            key={item.path}
-            to={`/${item.path}`}
-            className={item.isEmphasized ? "emphasized" : ""}
-          >
-            {item.title}
-          </Link>
+          <div key={item.path} className="top_level_item">
+            <Link
+              key={item.path}
+              to={`/${item.path}`}
+              className={item.isEmphasized ? " emphasized" : ""}
+            >
+              <span>{item.title}</span>
+              {hasSubmenu && <ChevronDown className="chevron" />}
+            </Link>
+            {hasSubmenu && (
+              <div className="submenu">
+                {item.subItems.map(subitem => (
+                  <Link key={subitem.path} to={`/${subitem.path}`}>
+                    {subitem.title}
+                  </Link>
+                ))}
+              </div>
+            )}
+          </div>
         );
       })}
     </nav>
