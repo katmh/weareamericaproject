c44582f7c48992d637fb7d7b42a86e7a9d4ac921
katmh <hello@katmh.com>
2022-07-19 01:06:01 -0700

    Mobile nav WIP

--- web/src/components/Nav.js
+++ web/src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { graphql, Link, useStaticQuery } from "gatsby";
 import ChevronDown from "../components/icons/ChevronDown";
 import "../styles/components/nav.scss";
@@ -19,46 +19,53 @@ const query = graphql`
   }
 `;
 
-/*
-nav
-  div.top_level_item [hover to make submenu appear]
-    a (gatsby <Link />)
-      span
-      (chevron)
-    div.submenu [display: none]
-      a.submenu_item
-*/
-
 const Nav = () => {
   const data = useStaticQuery(query);
   const { navItems } = data.sanitySiteSettings;
-
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const toggleNav = () => setIsNavOpen(!isNavOpen);
   return (
     <nav>
-      {navItems.map(item => {
-        const hasSubmenu = item.subItems.length > 0;
-        return (
-          <div key={item.path} className="top_level_item">
-            <Link
-              key={item.path}
-              to={`/${item.path}`}
-              className={item.isEmphasized ? " emphasized" : ""}
-            >
-              <span>{item.title}</span>
-              {hasSubmenu && <ChevronDown className="chevron" />}
-            </Link>
-            {hasSubmenu && (
-              <div className="submenu">
-                {item.subItems.map(subitem => (
-                  <Link key={subitem.path} to={`/${subitem.path}`}>
-                    {subitem.title}
-                  </Link>
-                ))}
-              </div>
-            )}
-          </div>
-        );
-      })}
+      <div className="mobile_only hamburger" onClick={toggleNav}>
+        ☰
+      </div>
+
+      <div
+        className={`mobile_only overlay${isNavOpen ? " overlay_open" : ""}`}
+        onClick={toggleNav}
+      />
+
+      <div
+        className={`items_container${isNavOpen ? " items_container_open" : ""}`}
+      >
+        <div className="mobile_only close_nav_button" onClick={toggleNav}>
+          x
+        </div>
+        {navItems.map(item => {
+          const hasSubmenu = item.subItems.length > 0;
+          return (
+            <div key={item.path} className="top_level_item">
+              <Link
+                key={item.path}
+                to={`/${item.path}`}
+                className={item.isEmphasized ? " emphasized" : ""}
+              >
+                <span>{item.title}</span>
+                {hasSubmenu && <ChevronDown className="chevron desktop_only" />}
+              </Link>
+              {hasSubmenu && (
+                <div className="submenu">
+                  {item.subItems.map(subitem => (
+                    <Link key={subitem.path} to={`/${subitem.path}`}>
+                      {subitem.title}
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
     </nav>
   );
 };
