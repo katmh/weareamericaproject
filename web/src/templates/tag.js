3f4eaa6ad20484b580259509a8f0213fb0ab1bdd
katmh <hello@katmh.com>
2022-08-02 12:52:31 -0700

    Update tag pages

--- web/src/templates/tag.js
+++ web/src/templates/tag.js
@@ -1,37 +1,40 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui"
-import Layout from "../components/Layout"
-import Gallery from "../components/Gallery"
-import StoryCard from "../components/StoryCard"
-import BackToAll from "../components/BackToAll"
+import { jsx } from "theme-ui";
+import Layout from "../components/Layout";
+import Gallery from "../components/Gallery";
+import StoryCard from "../components/StoryCard";
+import BackToAll from "../components/BackToAll";
 
-export default ({ pageContext: { edges, fieldValue } }) => (
+const TagPage = ({ pageContext: { nodes, tag } }) => (
   <Layout>
     <BackToAll name="stories" path="/stories" />
     <h1
       sx={{
         fontFamily: "heading",
         fontSize: 4,
-        mb: 3,
+        mb: 3
       }}
     >
       <span
         sx={{
-          color: "accent",
+          color: "accent"
         }}
       >
-        {fieldValue}{" "}
+        {tag}{" "}
       </span>
-      stories ({edges.length})
+      stories ({nodes.length})
     </h1>
     <Gallery n={3}>
-      {edges.map(edge => (
+      {nodes.map(node => (
         <StoryCard
-          photoUrl={edge.node.data.Photo[0].thumbnails.large.url}
-          title={edge.node.data.Story_Name}
-          author={edge.node.data.Author}
+          key={node.author}
+          photoUrl={node.photo.asset.url}
+          title={node.storyTitle}
+          author={node.author}
         />
       ))}
     </Gallery>
   </Layout>
-)
+);
+
+export default TagPage;
