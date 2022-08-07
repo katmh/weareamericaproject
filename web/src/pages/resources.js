85e4864837617f42e303def9b8f55560a2d61488
katmh <hello@katmh.com>
2022-08-07 00:14:22 -0700

    Use content from Sanity

--- web/src/pages/resources.js
+++ web/src/pages/resources.js
@@ -1,13 +1,31 @@
 import React from "react";
+import { graphql } from "gatsby";
+import { PortableText } from "@portabletext/react";
 import Layout from "../components/Layout";
 
-const ResourcesPage = () => {
+const ResourcesPage = ({ data }) => {
+  const rawText = data.allSanityPage.nodes[0].content[0]._rawContent;
   return (
     <Layout>
       <h1 className="heading large_heading">Resources</h1>
-      <p>Coming soon!</p>
+      <PortableText value={rawText} />
     </Layout>
   );
 };
+
+export const query = graphql`
+  query EducatorResourcesPageQuery {
+    allSanityPage(filter: { title: { eq: "Educator Resources" } }) {
+      nodes {
+        id
+        content {
+          ... on SanityTextSection {
+            _rawContent
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default ResourcesPage;
