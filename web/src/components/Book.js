95d506d641fd41451fc614d3046f1ec917f089ee
katmh <hello@katmh.com>
2023-08-27 22:44:26 -0400

    Remove links from book page

--- web/src/components/Book.js
+++ web/src/components/Book.js
@@ -1,25 +1,25 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
-import ButtonLink from "../components/ButtonLink";
+// import ButtonLink from "../components/ButtonLink";
 
-const Book = ({ title, image, url }) => {
+const Book = ({ title, image }) => {
   return (
     <article>
-      <a href={url}>
-        <img
-          src={image}
-          alt={"Cover of " + title}
-          sx={{
-            maxWidth: "100%",
-            mb: 3
-          }}
-        />
-      </a>
-      <center>
+      {/* <a href={url}> */}
+      <img
+        src={image}
+        alt={"Cover of " + title}
+        sx={{
+          maxWidth: "100%",
+          mb: 3
+        }}
+      />
+      {/* </a> */}
+      {/* <center>
         <ButtonLink destination={url}>
           Buy <i>{title}</i>
         </ButtonLink>
-      </center>
+      </center> */}
     </article>
   );
 };
