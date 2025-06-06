a96199b13248081eef0587899257d88017cdf254
katmh <hello@katmh.com>
2025-06-05 22:51:45 -0700

    Add cohort 7 (2025-2026)

--- studio/schemas/documents/teacher.js
+++ studio/schemas/documents/teacher.js
@@ -39,6 +39,7 @@ export default {
           "Cohort 4 (2022-2023)",
           "Cohort 5 (2023-2024)",
           "Cohort 6 (2024-2025)",
+          "Cohort 7 (2025-2026)",
         ],
       },
       validation: (Rule) => Rule.required(),
