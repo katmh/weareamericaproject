812804a2df52b07409006a8f383b5c4c041f1193
Kat Huang <katmhuang@gmail.com>
2026-06-06 17:32:54 -0400

    Upgrade Sanity Studio to v3 (#103)
    * Upgrade Sanity Studio to v3
    
    * Trigger Netlify rebuild

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,9 +1,3 @@
-// First, we must import the schema creator
-import createSchema from "part:@sanity/base/schema-creator";
-
-// Then import schema types from any plugins that might expose them
-import schemaTypes from "all:part:@sanity/base/schema-type";
-
 // document schemas
 import siteSettings from "./documents/siteSettings";
 import page from "./documents/page";
@@ -14,6 +8,7 @@ import school from "./documents/school";
 import teacher from "./documents/teacher";
 import book from "./documents/book";
 import newsItem from "./documents/newsItem";
+import post from "./documents/post";
 
 // Object types
 import guidesSection from "./objects/page-sections/guides";
@@ -30,40 +25,29 @@ import navItem from "./objects/navItem";
 import secondLanguageAudio from "./objects/secondLanguageAudio";
 import consent from "./objects/consent";
 
-// Then we give our schema to the builder and provide the result to Sanity
-export default createSchema({
-  // We name our schema
-  name: "blog",
-  // Then proceed to concatenate our our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    // The following are document types which will appear
-    // in the studio.
-    page,
-    guide,
-    siteSettings,
-    teamMember,
-    story,
-    school,
-    teacher,
-    book,
-    newsItem,
+export default [
+  page,
+  guide,
+  siteSettings,
+  teamMember,
+  story,
+  school,
+  teacher,
+  book,
+  newsItem,
+  post,
 
-    mainImage,
-    bodyPortableText,
-    bioPortableText,
-    excerptPortableText,
-    textSection,
-    partnersSection,
-    partner,
-    contactSection,
-    navItem,
-    secondLanguageAudio,
-    guidesSection,
-    booksSection,
-    consent,
-
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-  ]),
-});
+  mainImage,
+  bodyPortableText,
+  bioPortableText,
+  excerptPortableText,
+  textSection,
+  partnersSection,
+  partner,
+  contactSection,
+  navItem,
+  secondLanguageAudio,
+  guidesSection,
+  booksSection,
+  consent,
+];
